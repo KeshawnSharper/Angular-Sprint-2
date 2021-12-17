8eaa713797fa65ea1ddceb401315d3f46bd3297f
Keshawn Sharper <Ksharper@studentmba.org>
2021-12-17 10:47:33 -0500

    Update marvel.component.ts

--- AngularSprint2/src/app/marvel/marvel.component.ts
+++ AngularSprint2/src/app/marvel/marvel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-marvel',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./marvel.component.css']
 })
 export class MarvelComponent implements OnInit {
+  heroes : any[]= []
+  heroName = ""
+  resetHeroes : any[]= []
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get<any>(`https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json`).subscribe(data => {
+
+        this.heroes = data.filter((hero:any) => hero.biography.publisher === "Marvel Comics").filter((hero:any) => hero.biography.name !== "" && hero.biography.name !== "-").filter((hero:any) => hero.biography.fullName !== "" && hero.biography.fullName !== "-").filter((hero:any) => hero.biography.placeOfBirth !== "" && hero.biography.placeOfBirth !== "-").filter((hero:any) => hero.biography.firstAppearance !== "" && hero.biography.firstAppearance !== "-")
+        this.resetHeroes = data.filter((hero:any) => hero.biography.publisher === "Marvel Comics").filter((hero:any) => hero.biography.name !== "" && hero.biography.name !== "-").filter((hero:any) => hero.biography.fullName !== "" && hero.biography.fullName !== "-").filter((hero:any) => hero.biography.placeOfBirth !== "" && hero.biography.placeOfBirth !== "-").filter((hero:any) => hero.biography.firstAppearance !== "" && hero.biography.firstAppearance !== "-")
+        console.log(data)
+    })
   }
+  searchHeroes(){
+    console.log(this.heroName)
+    this.heroes = this.resetHeroes.filter(i => i.name.includes(this.heroName))
+    console.log(this.resetHeroes.filter(i => i.name.includes(this.heroName)))
+
+  }
+
+
+
+
+
+  
+
 
 }
