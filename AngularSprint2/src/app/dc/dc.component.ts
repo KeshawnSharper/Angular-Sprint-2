97704cb7a80ea34ab9128f6d2ad6f55cbe9c7c86
Keshawn Sharper <Ksharper@studentmba.org>
2021-12-17 10:42:49 -0500

    Update dc.component.ts

--- AngularSprint2/src/app/dc/dc.component.ts
+++ AngularSprint2/src/app/dc/dc.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dc.component.css']
 })
 export class DCComponent implements OnInit {
+  heroes : any[]= []
+  heroName = ""
+  resetHeroes : any[]= []
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get<any>(`https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json`).subscribe(data => {
+
+        this.heroes = data.filter((hero:any) => hero.biography.publisher === "DC Comics" ).filter((hero:any) => hero.biography.name !== "" && hero.biography.name !== "-").filter((hero:any) => hero.biography.fullName !== "" && hero.biography.fullName !== "-").filter((hero:any) => hero.biography.placeOfBirth !== "" && hero.biography.placeOfBirth !== "-").filter((hero:any) => hero.biography.firstAppearance !== "" && hero.biography.firstAppearance !== "-")
+        this.resetHeroes = data.filter((hero:any) => hero.biography.publisher === "DC Comics" ).filter((hero:any) => hero.biography.name !== "" && hero.biography.name !== "-").filter((hero:any) => hero.biography.fullName !== "" && hero.biography.fullName !== "-").filter((hero:any) => hero.biography.placeOfBirth !== "" && hero.biography.placeOfBirth !== "-").filter((hero:any) => hero.biography.firstAppearance !== ""&& hero.biography.firstAppearance !== "-")
+        console.log(data.filter((hero:any) => hero.biography.publisher === "DC Comics" ).filter((hero:any) => hero.biography.name !== "" && hero.biography.name !== "-").filter((hero:any) => hero.biography.fullName !== "" && hero.biography.fullName !== "-").filter((hero:any) => hero.biography.placeOfBirth !== "" && hero.biography.placeOfBirth !== "-").filter((hero:any) => hero.biography.firstAppearance !== ""&& hero.biography.firstAppearance !== "-"))
+    })
+  }
+  searchHeroes(){
+    console.log(this.heroName)
+    this.heroes = this.resetHeroes.filter(i => i.name.includes(this.heroName))
+    console.log(this.resetHeroes.filter(i => i.name.includes(this.heroName)))
   }
 
 }
